a932ad82fb0bc39caff22007f15741c6588da6e9
Tojba <yougotspam123@gmail.com>
2015-09-29 15:06:52 +0200

    added sdl

--- Project/Zombienator/main.cpp
+++ Project/Zombienator/main.cpp
@@ -3,8 +3,9 @@
 
 using namespace std;
 
-SDL_Surface* screen;
-SDL_Surface* image;
+SDL_Window *win = nullptr;
+SDL_Renderer *ren = nullptr;
+
 int main(int argc, char* args[])
 {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -12,20 +13,22 @@ int main(int argc, char* args[])
 		return 1;
 	}
 
-	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	win = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
 	if (win == nullptr) {
 		std::cout << "SDL_CreateWindow error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
 		return 1;
 	}
 
-	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (ren == nullptr) {
 		SDL_DestroyWindow(win);
 		std::cout << "SDL_CreateRenderer error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
 		return 1;
 	}
+
+	SDL_Delay(2000);
 
 	SDL_DestroyRenderer(ren);
 	SDL_DestroyWindow(win);
