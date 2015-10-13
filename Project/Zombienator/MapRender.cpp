cf605828cd0fc0a0f3b428be41f939f6e1baabce
Paul Laros <pjg.laros@gmail.com>
2015-10-13 22:19:35 +0200

    Uitlezen XML, 100 libraries geprobeerd (tmx loaders), niets werkt fatsoenlijk dus zelf schrijven

--- Project/Zombienator/MapRender.cpp
+++ Project/Zombienator/MapRender.cpp
@@ -4,13 +4,29 @@
 
 MapRender::MapRender(char* xml_url)
 {
-	cout << "gayy" << endl;
 
 	pugi::xml_document doc;
-
 	pugi::xml_parse_result result = doc.load_file(xml_url);
 
-	std::cout << "Load result: " << result.description() << ", mesh name: " << doc.child("mesh").attribute("name").value() << std::endl;
+	//std::cout << "Load result: " << result.description() << ", mesh name: " << doc.child("map").child("layer").attribute("name").value() << std::endl;
+	
+	// Get map
+	pugi::xml_node map = doc.child("map");
 
+	// Loop though elements
+	for (pugi::xml_node element = map.first_child(); element; element = element.next_sibling()) {
+
+		std::string elementName = element.name();
+
+		cout << elementName << endl;
+
+		if (elementName == "tileset") {
+			cout << "TILESET " << element.attribute("name").value() << endl;
+		}
+		else if (elementName == "layer") {
+			cout << "LAYER " << element.attribute("name").value() << endl;
+		}
+
+	}
 
 }
