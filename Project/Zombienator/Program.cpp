540d59d1bf19203b9eb24238cd7454a15c87c9f5
ninox92 <thomasreynders@hotmail.nl>
2015-10-15 20:30:07 +0200

    Merge remote-tracking branch 'refs/remotes/origin/dev' into f/drawcontainer
    Conflicts:
    	.gitignore
    	Project/Zombienator/Debug/Zombienator.ilk
    	Project/Zombienator/Debug/Zombienator.log
    	Project/Zombienator/Debug/Zombienator.pdb
    	Project/Zombienator/Debug/Zombienator.tlog/CL.command.1.tlog
    	Project/Zombienator/Debug/Zombienator.tlog/CL.read.1.tlog
    	Project/Zombienator/Debug/Zombienator.tlog/CL.write.1.tlog
    	Project/Zombienator/Debug/Zombienator.tlog/Zombienator.lastbuildstate
    	Project/Zombienator/Debug/Zombienator.tlog/link.command.1.tlog
    	Project/Zombienator/Debug/Zombienator.tlog/link.read.1.tlog
    	Project/Zombienator/Debug/Zombienator.tlog/link.write.1.tlog
    	Project/Zombienator/Debug/vc140.idb
    	Project/Zombienator/Debug/vc140.pdb
    	Project/Zombienator/Zombienator.sdf
    	Project/Zombienator/Zombienator.vcxproj
    	Project/Zombienator/Zombienator.vcxproj.filters

--- Project/Zombienator/Program.cpp
+++ Project/Zombienator/Program.cpp
@@ -5,15 +5,16 @@ Program::Program() {
 	if (InitComponents() == 0) {
 		cout << "Init complete" << endl;
 	}
+	if (InitJoystick() == 0) {
+		cout << "Init joystick" << endl;
+	}
 	if (Render() == 0) {
 		cout << "Done rendering" << endl;
-	}
+	} 
 }
 
 Program& Program::shared_program() {
-
 	static Program instance;
-
 	return instance;
 }
 
@@ -21,14 +22,15 @@ SDL_Renderer* Program::GetRenderer() {
 	return Sdl_Renderer;
 }
 
-TTF_Font * Program::GetFont()
-{
+TTF_Font * Program::GetFont() {
 	return Font;
 }
 
 int Program::Render() {
 	bool quit = false;
-	MenuScreen m = TestScreen{ Sdl_Renderer };
+	MenuScreen m = HomeScreen{ Sdl_Renderer };
+	//MenuScreen m = TestScreen{ Sdl_Renderer };
+
 	ScreenController::GetInstance().ChangeMenu(m);
 	while (!quit) {
 		//Handle events on queue 
@@ -43,8 +45,19 @@ int Program::Render() {
 				p.y = e.button.y;
 				ScreenController::GetInstance().GetCurrentMenu().ClickComponents(p);
 			}
+			else if (e.type == SDL_KEYDOWN) {
+				keyboardInputHandler->SetKey(e.key.keysym.sym, SDL_PRESSED);
+			}
+			else if (e.type == SDL_KEYUP) {
+				keyboardInputHandler->SetKey(e.key.keysym.sym, SDL_RELEASED);
+			}
+			else if (e.type == SDL_CONTROLLERBUTTONDOWN) {
+				controllerInputHandler->SetButton(e.cbutton, SDL_PRESSED);
+			}
+			else if (e.type == SDL_CONTROLLERBUTTONUP) {
+				controllerInputHandler->SetButton(e.cbutton, SDL_RELEASED);
+			}
 		}
-
 
 		SDL_SetRenderDrawColor(Sdl_Renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 		SDL_RenderClear(Sdl_Renderer);
@@ -54,14 +67,12 @@ int Program::Render() {
 		SDL_RenderPresent(Sdl_Renderer);
 	}
 
-	SDL_DestroyRenderer(Sdl_Renderer);
-	SDL_DestroyWindow(Sdl_Window);
-	SDL_Quit();
+
 	return 0;
 }
 
 int Program::InitComponents() {
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER) != 0) {
 		cerr << "SDL_Init error: " << SDL_GetError() << endl;
 		return 1;
 	}
@@ -92,5 +103,25 @@ int Program::InitComponents() {
 		SDL_Quit();
 		return 1;
 	}
+	keyboardInputHandler = &KeyboardInputHandler::GetInstance();
+	controllerInputHandler = &ControllerInputHandler::GetInstance();
 	return 0;
+}
+
+int Program::InitJoystick() {
+	for (int JoystickIndex = 0; JoystickIndex < SDL_NumJoysticks(); ++JoystickIndex)
+	{
+		if (!SDL_IsGameController(JoystickIndex))
+		{
+			continue;
+		}
+		controller = SDL_GameControllerOpen(JoystickIndex);
+	}
+	return 0;
+}
+Program::~Program() {
+	SDL_GameControllerClose(controller);
+	SDL_DestroyRenderer(Sdl_Renderer);
+	SDL_DestroyWindow(Sdl_Window);
+	SDL_Quit();
 }
---
+++ Project/Zombienator/SettingsButton.cpp
@@ -0,0 +1,47 @@
+#include "SettingsButton.h"
+#include "string"
+#include "MenuScreen.h"
+#include "HomeScreen.h"
+#include "ScreenController.h"
+#include "Program.h"
+#include "SDL_image.h"
+#include "SDL.h"
+
+SettingsButton::SettingsButton(SDL_Renderer& ren, std::string text, std::string img_url) : AbstractUIComponent(ren) {
+	TTF_Font* BlockCartoon = TTF_OpenFont("assets/fonts/Block-Cartoon.ttf", 1024);
+	SDL_Color White = { 255, 255, 255 };
+	char tab2[1024];
+	SDL_Surface *s = IMG_Load(strcpy(tab2, img_url.c_str()));
+
+	Image = SDL_CreateTextureFromSurface(&ren, s);
+	SDL_Surface* surfaceMessage = TTF_RenderText_Solid(BlockCartoon, strcpy(tab2, text.c_str()), White);
+	Message = SDL_CreateTextureFromSurface(&ren, surfaceMessage);
+}
+
+void SettingsButton::Draw(SDL_Renderer& ren) {
+
+	// Click Hitbox  
+	/*
+	SDL_SetRenderDrawColor(&ren, 0x00, 0x00, 0x00, 0xFF);
+	SDL_RenderFillRect(&ren, this);
+	*/
+
+	// Spritesheet Position
+	SDL_Rect srcrect;
+	srcrect.x = 286; srcrect.y = 0; srcrect.w = 82;	srcrect.h = 81;
+
+	// Screen Position
+	SDL_Rect dstrect;
+	dstrect.x = 1189; dstrect.y = 12; dstrect.w = 82; dstrect.h = 81;
+
+	// --
+	SDL_RenderCopy(&ren, Image, &srcrect, &dstrect);
+	SDL_RenderCopy(&ren, Message, 0, this);
+}
+
+void SettingsButton::ClickAction() 
+{
+	std::cout << "SettingsButton Clicked!" << std::endl;
+}
+
+SettingsButton::~SettingsButton() {}
