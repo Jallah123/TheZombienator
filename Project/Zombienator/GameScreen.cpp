b89888b53e3062eba1eb95906b085d9324efbab4
Sander de Haas <s.de.haas@outlook.com>
2015-10-21 20:44:47 +0200

    Geyl muziekske

--- Project/Zombienator/GameScreen.cpp
+++ Project/Zombienator/GameScreen.cpp
@@ -3,27 +3,58 @@
 
 GameScreen::GameScreen(SDL_Renderer* _ren, string path) : AbstractScreen(ren)
 {
-	 ren = _ren; 
-	 MapParser mp{};
-	 map = mp.ParseJsonMap(path);
-	 map.get()->setSprites(mp.GenerateSprites(path));
-	 SDL_Surface* s;
-	 s = IMG_Load(map->getImagePath().c_str());
-	 if (!s) {
-		 cout << "Error image load : " << IMG_GetError() << endl;
-		 return;
-	 }
-	 map.get()->setTexture(SDL_CreateTextureFromSurface(ren, s));
-	 SDL_FreeSurface(s);
-};
+	ren = _ren;
+	MapParser mp{};
+	map = mp.ParseJsonMap(path);
+	map.get()->setSprites(mp.GenerateSprites(path));
+	SDL_Surface* s;
+	s = IMG_Load(map->getImagePath().c_str());
+	if (!s) {
+		cout << "Error image load : " << IMG_GetError() << endl;
+		return;
+	}
+
+	//Load && play sound
+	loadSounds();
+	playSound();
+	map.get()->setTexture(SDL_CreateTextureFromSurface(ren, s));
+	SDL_FreeSurface(s);
+}
+
+void GameScreen::loadSounds() {
+
+	//more info : http://content.gpwiki.org/index.php/SDL_mixer:Tutorials:Playing_a_WAV_Sound_File
+	//todo dit dynamische maken -> kan pas bij levelfactory
+	sound = Mix_LoadWAV("assets/sounds/bgSound1.wav");
+	if (sound == NULL) {
+		cout << stderr << "Unable to load WAV file: %s\n" << Mix_GetError();
+	}
+}
+
+void GameScreen::playSound() {
+	int channel;
+	// first -1 will cause Mix_PlayChannel to play our sample on the first available sound channel.This is probably what you'll want to do, in most cases. If for some reason you wish to specify a certain channel for playback, you may pass an integer designating the desired channel.
+	//sound for the sound chunk
+	// Second -1 to play soundchunk only once. Passing -1 plays the sample on an infinite loop.
+	channel = Mix_PlayChannel(-1, sound, -1);
+	if (channel == -1) {
+		cout << stderr << "Unable to play WAV file: %s\n" << Mix_GetError();
+	}
+
+}
+
+void GameScreen::stopSound() {
+	Mix_FreeChunk(sound);
+	Mix_CloseAudio();
+}
 
 void GameScreen::Draw(SDL_Renderer& ren)
 {
 	vector<SDL_Rect*> sprites = map.get()->getSprites();
 	vector<MapLayer> layers = map.get()->getLayers();
 	vector<CollisionLayer> collisionLayers = map.get()->getCollisionLayers();
-	
-	for (int i = layers.size()-1; i >= 0; i--)
+
+	for (int i = layers.size() - 1; i >= 0; i--)
 	{
 		for (int x = 0; x < 24; x++)
 		{
@@ -47,23 +78,23 @@ void GameScreen::Draw(SDL_Renderer& ren)
 	}
 
 	/* For debugging purposes only */
-	SDL_SetRenderDrawColor(&ren, 0xFF, 0x00, 0x00, 0xFF);
-	SDL_Rect rectangle = { 80, 80, 50, 50 };
-	SDL_RenderDrawRect(&ren, &rectangle);
+	//SDL_SetRenderDrawColor(&ren, 0xFF, 0x00, 0x00, 0xFF);
+	//SDL_Rect rectangle = { 80, 80, 50, 50 };
+	//SDL_RenderDrawRect(&ren, &rectangle);
 
-	cout << "checkCollision() : " << map->checkCollision(80, 80, 50, 50) << endl;
+	//cout << "checkCollision() : " << map->checkCollision(80, 80, 50, 50) << endl;
 }
 
-void GameScreen::DrawRect(int x, int y, SDL_Rect* clip) { 
+void GameScreen::DrawRect(int x, int y, SDL_Rect* clip) {
 	//Set rendering space and render to screen 
-	SDL_Rect renderQuad = { y, x, 32, 32 }; 
+	SDL_Rect renderQuad = { y, x, 32, 32 };
 	//Set clip rendering dimensions 
-	if( clip != NULL ) { 
+	if (clip != NULL) {
 		renderQuad.w = clip->w;
-		renderQuad.h = clip->h; 
+		renderQuad.h = clip->h;
 	}
 	//Render to screen 
-	SDL_RenderCopy( ren, map.get()->getTexture(), clip, &renderQuad );
+	SDL_RenderCopy(ren, map.get()->getTexture(), clip, &renderQuad);
 }
 
 /* For debugging purposes only */
@@ -71,5 +102,5 @@ void GameScreen::DrawCollisionObject(int x, int y, int width, int height) {
 	//Set rendering space and render to screen 
 	SDL_Rect rectangle = { y, x, width, height };
 	//Render to screen
-	SDL_RenderDrawRect(ren, &rectangle );
+	SDL_RenderDrawRect(ren, &rectangle);
 }
--- Project/Zombienator/GameScreen.h
+++ Project/Zombienator/GameScreen.h
@@ -13,8 +13,12 @@ public:
 private:
 	unique_ptr<Map> map{ nullptr };
 	SDL_Renderer* ren;
+	Mix_Chunk *sound;
 	void DrawRect(int x, int y, SDL_Rect* clip);
 	void DrawCollisionObject(int x, int y, int width, int height);
+	void loadSounds();
+	void playSound();
+	void stopSound();
 protected:
 
 };
