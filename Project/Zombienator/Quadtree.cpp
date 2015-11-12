f85fa1d839737a26bcbc9395c6c144154d92534f
ninox92 <thomasreynders@hotmail.nl>
2015-11-12 15:02:27 +0100

    Quad tree working

--- Project/Zombienator/Quadtree.cpp
+++ Project/Zombienator/Quadtree.cpp
@@ -1,7 +1,9 @@
 #include "Quadtree.h"
 #include <SDL_rect.h>
 #include <stdio.h>
+#include <iostream>
 #include <SDL_render.h>
+#include <algorithm>
 #include "GameObject.h"
 
 Quadtree::Quadtree() :
@@ -98,11 +100,6 @@ vector<GameObject*> Quadtree::GetObjectsAt(double x, double y)
 
 void Quadtree::Display(SDL_Renderer *renderer)
 {
-	/*SDL_Rect rect;
-	rect.x = left;
-	rect.y = top;
-	rect.w = right - left;
-	rect.h = down - top;*/
 	SDL_SetRenderDrawColor(renderer, 48, 10, 36, 255);
 	SDL_RenderDrawRect(renderer, &rectangle);
 
@@ -116,65 +113,46 @@ void Quadtree::Display(SDL_Renderer *renderer)
 bool Quadtree::contains(GameObject *object)
 {
 	return SDL_HasIntersection(&rectangle, object->GetDestinationRect());
-	/*SDL_Rect* r2 = object->GetDestinationRect();
-	return !(r2->x > (rectangle.x + rectangle.w) ||
-		(r2->x + r2->w) < rectangle.x ||
-		r2->y > (rectangle.y + rectangle.h) ||
-		(r2->y + r2->h) < rectangle.y);
-*/
-	//return SDL_HasIntersection(&rectangle, object->GetDestinationRect());
-	/*return 	object->left > left &&
-		object->right < right &&
-		object->top > top &&
-		object->down < down;*/
 }
 
 bool Quadtree::contains(double x, double y)
 {
 	return (x >= rectangle.x && x <= (rectangle.x + rectangle.w)) &&
 		(y >= rectangle.y && y <= (rectangle.y + rectangle.h));
-	/*return 	(x >= left && x <= right) &&
-		(y >= top && y <= down);*/
 }
 
 void Quadtree::createLeaves()
 {
 	if (currentLevel >= maxLevel) return;
-	//nodes.reserve(4);
-	//Quadtree  ( left, right, top, down );
-	//Quadtree  (    0,  1024,   0,  768 ) ;
-	//               x,     w,   y,    h
+
 	int w = rectangle.w / 2;
 	int h = rectangle.h / 2;
 
 	SDL_Rect NW = { rectangle.x, rectangle.y, w, h };
-	SDL_Rect NE = { w, rectangle.y, w, h };
+	SDL_Rect NE = { rectangle.x + w, rectangle.y, w, h };
+	SDL_Rect SW = { rectangle.x, rectangle.y + h, w, h };
+	SDL_Rect SE = { rectangle.x + w, rectangle.y + h, w, h };
 
-	SDL_Rect SW = { rectangle.x, h, w, h };
-	SDL_Rect SE = { w, h, w, h };
-
+	//create new leave nodes
 	nodes.push_back(new Quadtree(NW, numObjectsToGrow, currentLevel + 1));
 	nodes.push_back(new Quadtree(NE, numObjectsToGrow, currentLevel + 1));
 	nodes.push_back(new Quadtree(SW, numObjectsToGrow, currentLevel + 1));
 	nodes.push_back(new Quadtree(SE, numObjectsToGrow, currentLevel + 1));
 
-
-	//nodes[NW] = Quadtree(left, (left + right) / 2, top, (top + down) / 2, numObjectsToGrow);
-	//nodes[NE] = Quadtree((left + right) / 2, right, top, (top + down) / 2, numObjectsToGrow);
-	//nodes[SW] = Quadtree(left, (left + right) / 2, (top + down) / 2, down, numObjectsToGrow);
-	//nodes[SE] = Quadtree((left + right) / 2, right, (top + down) / 2, down, numObjectsToGrow);
 	moveObjectsToLeaves();
 }
 
 void Quadtree::moveObjectsToLeaves()
 {
-	for (int n = 0; n < NodeCount; ++n) {
-		for (unsigned int m = 0; m < objects.size(); ++m) {
-			if (nodes.at(n)->contains(objects[m])) {
-				nodes.at(n)->AddObject(objects[m]);
-				objects.erase(objects.begin() + m);
-				--m;
+	vector<GameObject*> toErase = {};
+	for (auto& n : nodes) {
+		for (auto& o : objects) {
+			if (n->contains(o)) {
+				n->AddObject(o);
+				toErase.push_back(o);
 			}
 		}
 	}
+	for(auto& e : toErase)
+		objects.erase(std::remove(objects.begin(), objects.end(), e), objects.end());
 }
