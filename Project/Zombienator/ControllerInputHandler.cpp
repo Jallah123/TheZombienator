022b0626aa52db3bc363b5c4fea6e4c30a9c11b9
Tojba <yougotspam123@gmail.com>
2015-11-14 17:38:26 +0100

    added basic joystick handling, probably still needs some tweaking

--- Project/Zombienator/ControllerInputHandler.cpp
+++ Project/Zombienator/ControllerInputHandler.cpp
@@ -39,5 +39,13 @@ void ControllerInputHandler::SetAxis(SDL_ControllerAxisEvent axisEvent, bool b)
 	case SDL_CONTROLLER_AXIS_TRIGGERRIGHT:
 		inputContainer->SetKey(']', (axisEvent.value > 0));
 		break;
+	case SDL_CONTROLLER_AXIS_LEFTX:
+		inputContainer->SetKey('a', (axisEvent.value < -500));
+		inputContainer->SetKey('d', (axisEvent.value > 4000));
+		break;
+	case SDL_CONTROLLER_AXIS_LEFTY:
+		inputContainer->SetKey('w', (axisEvent.value < -500));
+		inputContainer->SetKey('s', (axisEvent.value > 4000));
+		break;
 	}
 }
