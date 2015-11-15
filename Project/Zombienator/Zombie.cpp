b8bed4782a9ae32ee8fc67a913c85eee14cdcb8f
ninox92 <thomasreynders@hotmail.nl>
2015-11-15 16:37:03 +0100

    zombie Numberutil

--- Project/Zombienator/Zombie.cpp
+++ Project/Zombienator/Zombie.cpp
@@ -1,6 +1,6 @@
 #include "Zombie.h"
 #include "CharacterContainer.h"
-#include <random>
+#include "NumberUtility.h"
 #include <string>
 
 using std::string;
@@ -15,11 +15,8 @@ Zombie::~Zombie()
 
 void Zombie::Init(DrawContainer * drawC, AnimateContainer * animC, MoveContainer * moveC, ActionContainer* actionC, CollideContainer* collideC, CharacterContainer* characterC, SDL_Renderer* ren)
 {
-	//Zombie doesn't have input from the InputContainer
-	std::random_device dev;
-	std::default_random_engine dre{ dev() };
-	std::uniform_int_distribution<int> dist{ 1, 6 };
-	int dice_roll = dist(dre);
+	//Zombie doesn't have input from the InputContainer	
+	int dice_roll = NumberUtility::RandomNumber(1, 6);
 
 	string basePath = "assets/images/spritesheets/";
 	string spriteSheet = std::to_string(dice_roll) + "zombie.png";
